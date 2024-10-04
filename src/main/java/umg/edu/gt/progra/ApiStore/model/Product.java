3a586049f38ed4f55a7eb23186d544e369affa1c
Walter Cordova <wcordova@beesion.com>
2024-10-03 20:55:27 -0600

    init JPA

--- src/main/java/umg/edu/gt/progra/ApiStore/model/Product.java
+++ src/main/java/umg/edu/gt/progra/ApiStore/model/Product.java
@@ -4,29 +4,76 @@
  */
 package umg.edu.gt.progra.ApiStore.model;
 
-import lombok.Data;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 /**
  *
  * @author wcordova
  */
-@Data
+@Entity
+@Table(name = "products")
 public class Product {
-    
+
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long productId;
-    private String name;
-    private double price;
-    private String description;
     
+    @Column(nullable = false)
+    private String name;
+    
+    @Column(nullable = false)
+    private double price;
+    
+    @Column(nullable = true)
+    private String description;
+
+    public Product() {
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     @Override
     public String toString() {
         return "Product{"
                 + "id =" + productId
                 + ", name = '" + name + "\""
                 + ", price = '" + price + "\""
-                + ", description = '" + description
-                ;
-    }   
-    
+                + ", description = '" + description;
+    }
 
 }
